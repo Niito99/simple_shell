47c3458b2ab739ab0defe5297100f836877b3533
Nazzy-1 <nnazirite@gmail.com>
2023-09-21 06:28:37 -0700

    move

---
+++ handle_cp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * copy - copies a string
+ * @env: strings delimited by ':'
+ *
+ * Return: copy of env
+ */
+
+char *copy(char *env)
+{
+	char *path = (char *)malloc(strlen(env) + 1);
+
+	strcpy(path, env);
+	return (path);
+}
