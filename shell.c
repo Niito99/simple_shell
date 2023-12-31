a8b46ffd0f56880beb79989b012321371054807c
Niito99 <therealokine1@gmai.com>
2023-12-31 12:45:41 +0000

    Check

--- shell.c
+++ shell.c
@@ -17,7 +17,7 @@ int main(void)
 		read_command(line, sizeof(line));
 		if (line[0] == 'e' && line[1] == 'x' && line[2] == 'i' && line[3] == 't')
 		{
-			if (line[4] == '\0' && line[5])
+			if (line[4] == '\0')
 				exit(0);
 			else
 				exit(2);
